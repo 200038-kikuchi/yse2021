daeba2c069da5da07745f207b877111435ce7aaa
sakatamitsutoshi <200275qz@yse-c.net>
2021-10-15 14:32:51 +0900

    1015

--- syukka.php
+++ syukka.php
@@ -13,23 +13,33 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①の処理を行う */) {
-	//②セッションを開始する
-}
+if (session_start()==PHP_SESSION_NONE) {
+	session_start();
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
+if (empty($_SESSION["login"]==false)){
+	$_SESSION["error2"] = "ログインしてください";
+	header('Location: login.php');
+}
 }
 
-//⑥データベースへ接続し、接続情報を変数に保存する
-
-//⑦データベースで使用する文字コードを「UTF8」にする
+$db_name = "zaiko2021_yse";
+$db_host = "localhost";
+$db_charset ="utf8";
+$dsn ="mysql:dbname={$db_name};host={$db_host};charset={$db_charset}";
+$user ="zaiko2021_yse";
+$pass ="2021zaiko";
+try{
+	$pdo = new PDO($dsn,$user,$pass);
+}catch(PDOException $e){
+	echo "接続エラー";
+}
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
+if(empty($_POST["books"])){
+	$_SESSION["success"]="出荷する商品が選択されていません";
 	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
+	header('Location: zaiko_ichiran.php');
 	//⑩在庫一覧画面へ遷移する。
 }
 
@@ -41,6 +51,10 @@ function getId($id,$con){
 	 */
 
 	//⑫実行した結果から1レコード取得し、returnで値を返す。
+	$sql = "SELECT * FROM books cities WHERE id = ";
+	return ;
+
+
 }
 ?>
 <!DOCTYPE html>
@@ -73,9 +87,9 @@ function getId($id,$con){
 		/*
 		 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 		 * 設定されていた場合はif文の中に入る。
-		 */ 
-		if(/* ⑬の処理を書く */){
-			//⑭SESSIONの「error」の中身を表示する。
+		 */
+		if(isset($_SESSION["error"])){
+			echo $_SESSION["error"];
 		}
 		?>
 		</div>
@@ -96,17 +110,17 @@ function getId($id,$con){
 				/*
 				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 				 */
-				foreach(/* ⑮の処理を書く */){
+				foreach($books as $book){
 					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
 				?>
-				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
+				<input type="hidden" value="<?php //echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
 				<tr>
-					<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
+				<td><?php //echo	$book["id"] ;?></td>
+					<td><?php //echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
 					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 				</tr>
 				<?php
