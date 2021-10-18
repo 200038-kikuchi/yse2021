d2eb57f28d95c424419a2f53f332cbbc7bd4985a
200019Uemura <200019bg@yse-c.net>
2021-10-18 13:43:52 +0900

    waakaaa

--- nyuka.php
+++ nyuka.php
@@ -14,21 +14,13 @@
  * 一致した場合はif文の中に入る。
  */
 if (session_status()==PHP_SESSION_NONE) {
-	//②セッションを開始する
 	session_start();
 }
-
-
-//③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
 if (!$_SESSION["login"]){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
 	$_SESSION["error2"]="ログインしてください";
-	//⑤ログイン画面へ遷移する。
 	header("Location:login.php");
 }
 
-//⑥データベースへ接続し、接続情報を変数に保存する
-//⑦データベースで使用する文字コードを「UTF8」にする
 $db_name="zaiko2021_yse";
 $db_host="localhost";
 $db_port="3306";
@@ -41,26 +33,16 @@ try{
 	echo "接続エラー";
 }
 
-//⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
 if(empty($_POST["books"])){
-	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
 	$_SESSION["success"] ="入荷する商品が選択されていません";
-	//⑩在庫一覧画面へ遷移する。
 	header("Location:zaiko_ichiran.php");
 }
 
 function getId($id,$con){
-	/* 
-	 * ⑪書籍を取得するSQLを作成する実行する。
-	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
-	 * SQLの実行結果を変数に保存する。
-	 */
 	$sql = "SELECT * FROM books WHERE id ={$id}";
 	$stmt = $con->queli($sql);
 	$stmt->execute(["id" => $id]);
-
-	//⑫実行した結果から1レコード取得し、returnで値を返す。
-	return $stmt->fetch();
+	return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -72,12 +54,9 @@ function getId($id,$con){
 	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
 <body>
-	<!-- ヘッダ -->
 	<div id="header">
 		<h1>入荷</h1>
 	</div>
-
-	<!-- メニュー -->
 	<div id="menu">
 		<nav>
 			<ul>
@@ -88,15 +67,9 @@ function getId($id,$con){
 
 	<form action="nyuka_kakunin.php" method="post">
 		<div id="pagebody">
-			<!-- エラーメッセージ -->
 			<div id="error">
 			<?php
-			/*
-			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
-			 * 設定されていた場合はif文の中に入る。
-			 */ 
 			if(isset($_SESSION["error"])){
-				//⑭SESSIONの「error」の中身を表示する。
 				echo '<p>'.$_SESSION["error"].'</p>';
 				$_SESSION["error"]="";
 
@@ -117,21 +90,18 @@ function getId($id,$con){
 						</tr>
 					</thead>
 					<?php 
-					/*
-					 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
-					 */
     				foreach($_POST["books"] as $id){
-    					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
+
 						$book = getId($id,$pdo);
 					?>
 					<input type="hidden" value="<?php echo	$book["id"];?>" name="books[]">
 					<tr>
-						<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */$book["id"];?></td>
-						<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */$book["title"];?></td>
-						<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */$book["author"];?></td>
-						<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */$book["salesDate"];?></td>
-						<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */$book["price"];?></td>
-						<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */$book["stock"];?></td>
+						<td><?php echo	$book["id"];?></td>
+						<td><?php echo	$book["title"];?></td>
+						<td><?php echo	$book["author"];?></td>
+						<td><?php echo	$book["salesDate"];?></td>
+						<td><?php echo	$book["price"];?></td>
+						<td><?php echo	$book["stock"];?></td>
 						<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 					</tr>
 					<?php
@@ -142,7 +112,6 @@ function getId($id,$con){
 			</div>
 		</div>
 	</form>
-	<!-- フッター -->
 	<div id="footer">
 		<footer>株式会社アクロイト</footer>
 	</div>
