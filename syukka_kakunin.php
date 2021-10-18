591210a6592793b6ab6aea83ed78e0963ddb8652
200038-kikuchi <200038up@yse-c.net>
2021-10-18 14:22:25 +0900

    ㊵まで

--- syukka_kakunin.php
+++ syukka_kakunin.php
@@ -30,7 +30,7 @@ function updateByid($id,$con,$total){
 	 * 引数で受け取った$totalの値で在庫数を上書く。
 	 * その際にWHERE句でメソッドの引数に$idに一致する書籍のみ取得する。
 	 */
-	$sql ="UPDATE books SET stock = $total WERE :id = id";
+	$sql ="UPDATE books SET stock = $total WHERE :id = id";
 	$stmt = $con->prepare($sql);
 	$stmt-> execute([":id" => $id]);
 }
