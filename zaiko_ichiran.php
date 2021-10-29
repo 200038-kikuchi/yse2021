c13a2fe0e3415ba9c8bf91ad2171ebea1b138ed1
sakatamitsutoshi <200275qz@yse-c.net>
2021-10-29 13:51:58 +0900

    1029修正

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -76,7 +76,7 @@ while ($book = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			<div id="left">
 				<p id="ninsyou_ippan">
 					<?php
-						echo @$_SESSION["account_name"];
+						echo $_SESSION["name"];
 					?><br>
 					<button type="button" id="logout" onclick="location.href='logout.php'">ログアウト</button>
 				</p>
